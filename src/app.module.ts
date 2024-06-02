03d4b1e024da67ccda743d60267d25b338afbf3e
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-06-02 21:51:36 +0200

    Re-add database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import configuration from "./config/configuration";
+import { UsersModule } from "./users/users.module";
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: undefined,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: config.get<string>('database.host'),
+          port: config.get<number>('database.port'),
+          username: config.get<string>('database.user'),
+          password: config.get<string>('database.password'),
+          database: config.get<string>('database.name'),
+          synchronize: config.get<boolean>('database.synchronize'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          migrations: ['migrations/*.js'],
+        };
+      },
+    }),
+    UsersModule,
+  ],
+})
 export class AppModule {}
