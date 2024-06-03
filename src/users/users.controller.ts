07c1c634d6cbe2cd839a6a8d79d4794bebb9bbb5
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-06-03 18:13:45 +0200

    Exclude "password" from users response

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
