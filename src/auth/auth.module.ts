9383671f84dceb007b1f2cbfddc79e48ba76c615
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-06-03 14:04:55 +0200

    Implement LocalStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
