f24aa8b4e7244c5e0343c2205fcb65956cc959d4
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-06-03 14:04:29 +0200

    Implement validateUser Method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { LoginDto } from './dtos/login.dto';
+import { Repository } from 'typeorm';
+import { User } from '../users/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class AuthService {
-  login(loginDto: LoginDto) {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  login() {
     return 'LOGIN';
+  }
+
+  async validateUser(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+
+    const user = await this.usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      return null;
+    }
+
+    // TODO: Check password with hashed password
+    const match = password === user.password;
+
+    if (!match) {
+      return null;
+    }
+
+    return user;
   }
 }
