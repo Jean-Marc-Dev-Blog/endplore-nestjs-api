72e1715e9b4e005a1d5f3b5506256d9dcacb6128
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-06-03 18:44:00 +0200

    Clean Up

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -1,5 +1,4 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User } from '../entities/user.entity';
 
 export const CurrentUser = createParamDecorator(
   (data: string, context: ExecutionContext) => {
