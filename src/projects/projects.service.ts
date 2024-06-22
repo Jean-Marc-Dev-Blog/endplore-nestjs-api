cb6628cfdcbd9112ac64472bb90c53ca42a71473
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-06-22 21:15:55 +0200

    Create "getProjects" in ProjectsService

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Project } from './entities/project.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  constructor(
+    @InjectRepository(Project)
+    private readonly projectsRepository: Repository<Project>,
+  ) {}
+
+  getProjects(userId: string) {
+    return this.projectsRepository.find({ where: { user: { id: userId } } });
+  }
+}
